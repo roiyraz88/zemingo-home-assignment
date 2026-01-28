b932d90a26b5f42a9acd36f79b9fb78f00768429
Roie Raz <roiyraz88@cs.colman.ac.il>
2026-01-28 10:03:35 +0200

    DB configuration

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,11 @@
+import dotenv from "dotenv";
+dotenv.config();
 import app from "./app";
+import {connectDB} from "../src/config/db"
 
+connectDB();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-
-
-
-
-
-  
 });
